e33d3ffd6aae5c7979ec0849d623b9425664eb91
Caroline Konrad <ckonrad@live.unc.edu>
2022-07-13 12:24:12 -0400

    Finishing all models

--- migrations/20220713155828-create-meet-greet.js
+++ migrations/20220713155828-create-meet-greet.js
@@ -2,35 +2,28 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('meetGreets', {
-      id: {
+      meet_greet_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      meet_greet_id: {
-        type: Sequelize.INTEGER
-      },
       event_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       band_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       meet_start_time: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false
       },
       meet_end_time: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
     });
   },
   async down(queryInterface, Sequelize) {
