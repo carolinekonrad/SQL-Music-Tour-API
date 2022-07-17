5fed062bcadbd945fe6412a616a5e2e53e084858
Caroline Konrad <ckonrad@live.unc.edu>
2022-07-17 14:32:18 -0400

    Updating routes

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -9,28 +9,13 @@ const { Op } = require('sequelize')
 bands.get('/', async (req, res) => {
     try {
         const foundBands = await Band.findAll({
-            order: [[ 'available_start_time', 'ASC']],
+            order: [ [ 'available_start_time', 'ASC' ] ],
             where: {
-                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%`}
-            },
-            include: [{
-                model: meetGreet, 
-                as: "meet_greets",
-                include: { model: Event, as: "event", where: {
-                    name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%`}
-                } }
-            },
-            {
-                model: setTime,
-                as: "set_times",
-                include: { model: Event, as: "event", where: {
-                    name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%`}
-                } }
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
             }
-            ]
         })
         res.status(200).json(foundBands)
-    } catch(error) {
+    } catch (error) {
         res.status(500).json(error)
     }
 })
@@ -39,8 +24,34 @@ bands.get('/', async (req, res) => {
 bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: {name: req.params.name}
-        }) 
+            where: { name: req.params.name },
+            include: [
+                { 
+                    model: meetGreet, 
+                    as: "meet_greets", 
+                    attributes: { exclude: ["band_id", "event_id"] },
+                    include: { 
+                        model: Event, 
+                        as: "event", 
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } } 
+                    }
+                },
+                { 
+                    model: setTime, 
+                    as: "set_times",
+                    attributes: { exclude: ["band_id", "event_id"] },
+                    include: { 
+                        model: Event, 
+                        as: "event", 
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } } 
+                    }
+                }
+            ],
+            order: [
+                [{ model: meetGreet, as: "meet_greets" }, { model: Event, as: "event" }, 'date', 'DESC'],
+                [{ model: setTime, as: "set_times" }, { model: Event, as: "event" }, 'date', 'DESC']
+            ]
+        })
         res.status(200).json(foundBand)
     } catch (error) {
         res.status(500).json(error)
--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -1,7 +1,7 @@
 //DEPENDENCIES
 const events = require('express').Router()
 const db = require('../models')
-const { Event } = db
+const { Event, meetGreet, setTime, Stage, Band } = db
 const { Op } = require('sequelize')
 
 //ROUTES
@@ -21,10 +21,35 @@ events.get('/', async (req, res) => {
 })
 
 //FIND A SPECIFIC EVENT
-events.get('/:id', async (req, res) => {
+events.get('/:name', async (req, res) => {
     try {
         const foundEvent = await Event.findOne({
-            where: { event_id: req.params.id }
+            where: { name: req.params.name },
+            include: [
+                { 
+                    model: meetGreet, 
+                    as: "meet_greets", 
+                    attributes: { exclude: [ "event_id", "band_id" ] },
+                    include: {
+                         model: Band, 
+                         as: "band", 
+                    } 
+                },
+                { 
+                    model: setTime, 
+                    as: "set_times",
+                    attributes: { exclude: [ "event_id", "stage_id", "band_id" ] },
+                    include: [
+                        { model: Band, as: "band" },
+                        { model: Stage, as: "stage" }
+                    ]
+                },
+                { 
+                    model: Stage, 
+                    as: "stages",
+                    through: { attributes: [] }
+                }
+            ]
         })
         res.status(200).json(foundEvent)
     } catch (error) {
